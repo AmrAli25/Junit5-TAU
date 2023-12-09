a47629dd123039871680f9a7d58593bd469feadd
Amr Ali <amraliahmed25@gmail.com>
2023-12-09 20:05:21 +0200

    Junit5 course TAU - Ch3&4

---
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -0,0 +1,23 @@
+package junit5tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ParameterizedTests {
+
+    @ParameterizedTest(name = "Run : {index} - value : {arguments}")
+    @ValueSource(ints = {1, 5, 60})
+    void intValues(int theparam){
+        System.out.println("theparam = " + theparam);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"FirstString", "SecondString"})
+    void strinfValues(String theparam){
+        System.out.println("theparam = " + theparam);
+    }
+}
